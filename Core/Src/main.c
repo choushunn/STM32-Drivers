3b45a2fb9c53258d6bd94337d773e91d817b48fe
f2007wu <2711038300@qq.com>
2026-05-10 12:08:23 +0800

    add：新增了沁和智能TS-300B浊度探头的驱动

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -197,9 +197,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
-  MX_I2C2_Init();
   MX_USART1_UART_Init();
   MX_TIM3_Init();
+  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start(&htim3);
 
